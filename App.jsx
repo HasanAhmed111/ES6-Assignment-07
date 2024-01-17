c00fe16cedd66221ee62b9e02403b0604f2a4ec5
HasanAhmed111 <144584937+HasanAhmed111@users.noreply.github.com>
2024-01-17 17:40:09 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Todo_list } from "./components/List";
+import { Navbar } from "./components/Navbar";
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+<Todo_list/>
+    </>
+  );
+};
+
+export default App;
